a203e79d7f4fbc1e68491bffdb1c850051a2e134
Natsu <natsu1052@hotmail.com>
2018-04-17 22:49:11 +0800

    Add input and tab

--- src/js/views/Home.vue
+++ src/js/views/Home.vue
@@ -1,9 +1,28 @@
 <template>
-  <div class="container">
-    <div class="row col-sm-6">
-      <div class="col-sm">All</div>
-      <div class="col-sm">Doing</div>
-      <div class="col-sm">Done</div>
+  <div class="content">
+    <div class="col-sm-6">
+      <form>
+        <div class="row main-input-group">
+          <input class="col form-control" v-model.trim="newTask" placeholder="New Task"/>
+          <button class="btn btn-primary" @click="onAddTask">Add Task</button>
+        </div>
+      </form>
+      <div class="row tab-group">
+        <div class="col tab-title" :class="{active: filter === 'all'}"
+          @click="onClickTab('all')">
+          All
+        </div>
+        <div class="col tab-title" :class="{active: filter === 'doing'}"
+          @click="onClickTab('doing')">
+          Doing
+        </div>
+        <div class="col tab-title" :class="{active: filter === 'done'}"
+          @click="onClickTab('done')">
+          Done
+        </div>
+      </div>
+      <div>
+      </div>
     </div>
   </div>
 </template>
@@ -13,11 +32,18 @@
     name: 'Home',
     data() {
       return {
-
+        newTask: '',
+        tasks: [],
+        filter: 'all'
       };
     },
     methods: {
+      onAddTask() {
 
+      },
+      onClickTab(tab) {
+        this.filter = tab;
+      }
     }
   }
 </script>
